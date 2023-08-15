d7a0cb702528c9a958f8f103996b030ee2e1a3ab
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-15 16:23:57 +0300

    Add users table migration

---
+++ migrations/20230815125252-create-user.js
@@ -0,0 +1,53 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        type: Sequelize.STRING(100),
+        field: 'full_name',
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      isMale: {
+        type: Sequelize.BOOLEAN,
+        field: 'is_male'
+      },
+      birthday: {
+        type: Sequelize.DATEONLY
+      },
+      bonusAmount: {
+        type: Sequelize.DECIMAL(9, 2),
+        field: 'bonus_amount',
+        defaultValue: 0
+      },
+      createdAt: {
+        allowNull: false,
+        field: 'created_at',
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        field: 'updated_at',
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
