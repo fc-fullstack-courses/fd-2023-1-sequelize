d6b5c000b36753365eb78568a3328661034b2aba
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-16 14:48:27 +0300

    Add getUsers route

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -11,3 +11,33 @@ module.exports.createUser = async (req, res, next) => {
     next(error);
   }
 }
+
+module.exports.getUsers = async (req, res, next) => {
+  try {
+    
+    // SELECT * FROM users;
+    const users = await User.findAll();
+
+    // SELECT fullName, isMale FROM users;
+    // const users = await User.findAll({
+    //   attributes: ['fullName', 'isMale']
+    // });
+
+    // // SELECT fullName, bonusAmount as money FROM users;
+    // const users = await User.findAll({
+    //   attributes: ['fullName', ['bonus_amount', 'money']]
+    // });
+
+    // SELECT id, fullName, email, isMale, ... as money FROM users;
+    // const users = await User.findAll({
+    //   attributes: {
+    //     exclude: ['password']
+    //   }
+    // });
+
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
+}
+
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,5 +2,6 @@ const userRouter = require('express').Router();
 const UserController = require('../controllers/users.controller');
 
 userRouter.post('/', UserController.createUser);
+userRouter.get('/', UserController.getUsers);
 
 module.exports = userRouter;
