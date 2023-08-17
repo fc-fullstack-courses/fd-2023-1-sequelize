7507e20d637b0af3571518f111cc4f3721c0a0c9
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-17 16:11:49 +0300

    Add Task model

---
+++ models/task.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Task.init({
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    isDone: {
+      type: DataTypes.BOOLEAN,
+      field: 'is_done',
+      allowNull: false,
+      defaultValue: false
+    },
+    deadline: {
+      type: DataTypes.DATE,
+      validate: {
+        isDate: true
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Task',
+    tableName: 'tasks',
+    underscored: true
+  });
+  return Task;
+};
