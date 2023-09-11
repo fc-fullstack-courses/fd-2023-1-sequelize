d788375c0771dc135dc19776f20b4ba05d8d5aa0
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-11 15:03:34 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routers');
 const basicErrorHandler = require('./middlewares/errors/basic');
-const sequelizeErrorHandler  = require('./middlewares/errors/sequelizeHandlers');
+const sequelizeErrorHandler = require('./middlewares/errors/sequelizeHandlers');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(sequelizeErrorHandler);
